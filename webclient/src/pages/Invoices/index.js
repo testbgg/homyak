8283adfecbd80724f5a6d9086e1d976cfb7f67ed
John Doe <johndoe@example.com>
2018-11-17 00:51:10 +0300

    frontend add modal table

--- webclient/src/pages/Invoices/index.js
+++ webclient/src/pages/Invoices/index.js
@@ -1,7 +1,64 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Button, Icon, Modal } from "antd";
+import { Button, Icon, Modal, Table } from "antd";
 import "./Invoices.sass";
+
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    render: text => <a href="javascript:;">{text}</a>
+  },
+  {
+    title: "Age",
+    dataIndex: "age"
+  },
+  {
+    title: "Address",
+    dataIndex: "address"
+  }
+];
+const data = [
+  {
+    key: "1",
+    name: "John Brown",
+    age: 32,
+    address: "New York No. 1 Lake Park"
+  },
+  {
+    key: "2",
+    name: "Jim Green",
+    age: 42,
+    address: "London No. 1 Lake Park"
+  },
+  {
+    key: "3",
+    name: "Joe Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park"
+  },
+  {
+    key: "4",
+    name: "Disabled User",
+    age: 99,
+    address: "Sidney No. 1 Lake Park"
+  }
+];
+
+// rowSelection object indicates the need for row selection
+const rowSelection = {
+  onChange: (selectedRowKeys, selectedRows) => {
+    console.log(
+      `selectedRowKeys: ${selectedRowKeys}`,
+      "selectedRows: ",
+      selectedRows
+    );
+  },
+  getCheckboxProps: record => ({
+    disabled: record.name === "Disabled User", // Column configuration not to be checked
+    name: record.name
+  })
+};
 
 export default class Invoices extends Component {
   state = {
@@ -57,7 +114,13 @@ export default class Invoices extends Component {
               confirmLoading={confirmLoading}
               onCancel={this.handleCancel}
             >
-              <p>ТУТ СПИСОК</p>
+              <p>
+                <Table
+                  rowSelection={rowSelection}
+                  columns={columns}
+                  dataSource={data}
+                />
+              </p>
             </Modal>
           </div>
           <div className="invoices__list">
