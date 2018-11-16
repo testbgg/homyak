03c9697e93ab71cce2c0dd8343ea147e438c7b7c
John Doe <johndoe@example.com>
2018-11-16 23:23:04 +0300

    frontend add Invoices

--- webclient/src/App.js
+++ webclient/src/App.js
@@ -8,20 +8,23 @@ import {
 import Cards from "./pages/Cards";
 import Login from "./pages/Login";
 import Invoices from "./pages/Invoices";
+import { Row } from "antd";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Router>
-          <Switch>
-            <Route path="/login" component={Login} />
-            <Route path="/invoices" component={Invoices} />
-            <Route path="/invoices/:invoiceId" component={Cards} />
-            <Redirect exact from="/" to="/login" />
-          </Switch>
-        </Router>
+      <div>
+        <Row type="flex">
+          <Router>
+            <Switch>
+              <Route path="/login" component={Login} />
+              <Route path="/invoices" component={Invoices} />
+              <Route path="/invoices/:invoiceId" component={Cards} />
+              <Redirect exact from="/" to="/login" />
+            </Switch>
+          </Router>
+        </Row>
       </div>
     );
   }
--- webclient/src/pages/Invoices/index.js
+++ webclient/src/pages/Invoices/index.js
@@ -1,7 +1,20 @@
 import React, { Component } from "react";
+import "./Invoices.sass";
 
 export default class Invoices extends Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <header>
+          <h1>Корпоративные карты</h1>
+        </header>
+        <section>
+          <div className="invoices__to-card-invoice">
+            <button>Привязать Л/C</button>
+          </div>
+          <div className="invoices__list"></div>
+        </section>
+      </div>
+    );
   }
 }
