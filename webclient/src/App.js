49e5b7aa43a64479090d71bcb95ccccd259a60ae
John Doe <johndoe@example.com>
2018-11-16 20:37:04 +0300

    frontend add react-router

--- webclient/src/App.js
+++ webclient/src/App.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import "./App.css";
+
+const Login = () => <div> Логин </div>;
+const Cards = () => <div> Пароль </div>;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router>
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/cards" component={Cards} />
+            <Redirect exact from="/" to="/login" />
+          </Switch>
+        </Router>
       </div>
     );
   }
