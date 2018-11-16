6a9df9eee3029464a74bd5b840cc68ac60bb9dd5
Nikita Shaldenkov <nikita.shaldenkov@bostongene.com>
2018-11-16 19:26:41 +0300

    init

---
+++ src/main/java/com/bgdevs/madness/HomyakApplication.java
@@ -0,0 +1,12 @@
+package com.bgdevs.madness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomyakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomyakApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bgdevs/madness/HomyakApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bgdevs.madness;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HomyakApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
