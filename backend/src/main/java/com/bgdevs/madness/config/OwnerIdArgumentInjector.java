09f641eab8fe2551dbee4c3a0661058e7ab08c5c
Nikita Shaldenkov <shaldnikita2@yandex.ru>
2018-11-17 04:45:25 +0300

    123

---
+++ backend/src/main/java/com/bgdevs/madness/config/OwnerIdArgumentInjector.java
@@ -0,0 +1,30 @@
+package com.bgdevs.madness.config;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+/**
+ * @author Nikita Shaldenkov
+ */
+@Service
+public class OwnerIdArgumentInjector implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterType().equals(Long.class)
+                && parameter.getParameterName().equals("ownerId");
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        return Long.valueOf(((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername());
+    }
+}
+
