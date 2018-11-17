7f2fb32d1b166b61c79dd1d6fdeee3f31a0a9ecc
Nikita Shaldenkov <shaldnikita2@yandex.ru>
2018-11-17 04:22:16 +0300

    123

---
+++ backend/src/main/java/com/bgdevs/madness/service/AuthService.java
@@ -0,0 +1,34 @@
+package com.bgdevs.madness.service;
+
+import com.bgdevs.madness.dao.entities.User;
+import com.bgdevs.madness.dao.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Optional;
+
+/**
+ * @author Nikita Shaldenkov
+ */
+@Service
+public class AuthService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+        if (!user.isPresent()) {
+            throw new UsernameNotFoundException("No user present with username: " + username);
+        } else {
+            return new org.springframework.security.core.userdetails.User(user.get().getUsername(), user.get().getPassword(),
+                    Collections.singletonList(new SimpleGrantedAuthority("USER")));
+        }
+    }
+}
