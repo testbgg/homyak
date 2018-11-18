6a1a79b1feef6c691179620982a9c819398ae0da
Valeriy Knyazhev <valeriy.knyazhev@yandex.ru>
2018-11-18 10:25:01 +0300

    add constraint on employee in credit card

--- backend/src/main/java/com/bgdevs/madness/service/card/model/CreateCardModel.java
+++ backend/src/main/java/com/bgdevs/madness/service/card/model/CreateCardModel.java
@@ -3,9 +3,12 @@ package com.bgdevs.madness.service.card.model;
 import lombok.Data;
 
 import javax.annotation.Nullable;
+import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import java.math.BigDecimal;
+
+import static com.bgdevs.madness.dao.entities.card.CardType.CREDIT;
 
 /**
  * @author Nikita Shaldenkov
@@ -28,4 +31,10 @@ public class CreateCardModel {
 
     @Nullable
     private BigDecimal monthLimit;
+
+    @AssertTrue(message = "Credit card must be requested for employee.")
+    private boolean isValid() {
+        return !this.type.equals(CREDIT.getLabel()) || this.employeeId != null;
+    }
+
 }
